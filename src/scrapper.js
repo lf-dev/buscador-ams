5f5e8c7e4ee2023f78f433fcff36696ee48f42b0
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-04-23 17:59:35 -0300

    instanciacao de credenciado e merge em scrapper

--- src/scrapper.js
+++ src/scrapper.js
@@ -1,36 +1,51 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var _ = require('lodash');
 var Parser = require('./Parser.js');
+var Credenciado = require('./Credenciado.js');
 
 var url = 'https://seguro2.petrobras.com.br/buscaams/busca.do';
 var formData = {
-  estado: 'RJ',
-  cidade: 'ANGRA DOS REIS',
-  atendimento: 'true',
-  method: 'buscar'
+    estado: 'RJ',
+    cidade: 'ARMACAO DOS BUZIOS',
+    atendimento: 'true',
+    method: 'buscar'
 }
 
-request.post({url:url, formData: formData}, function(err, httpResponse, body) {
-  if (err) {
-    return console.error('upload failed:', err);
-  }
-
-  var $ = cheerio.load(body);
-  var parser = new Parser($);
-
-  var total = 0;
-  var trs = $('tr');
-  for(var i=0; i<trs.length; i++){
-
-    var tr = $(trs[i]);
-
-    if(parser.isData(tr)){
-      total++;
-      console.log("---------------------");
-      console.log(parser.toJSON(tr));
+request.post({url: url, formData: formData}, function (err, httpResponse, body) {
+    if (err) {
+        return console.error('upload failed:', err);
     }
-  }
-  console.log("total " + total);
 
-  console.log(trs.length);
+    var $ = cheerio.load(body);
+    var parser = new Parser($);
+
+    var total = 0;
+    var trs = $('tr');
+    let credenciados = [];
+    for (var i = 0; i < trs.length; i++) {
+
+        var tr = $(trs[i]);
+
+        if (parser.isData(tr)) {
+            total++;
+            let json = parser.toJSON(tr);
+            let novoCredenciado = new Credenciado(json);
+
+            let mesmoCredenciado = _.find(credenciados, function(credenciadoExistente) {
+                return novoCredenciado.equals(credenciadoExistente);
+            });
+
+            if(mesmoCredenciado == null){
+                credenciados.push(novoCredenciado);
+            }else {
+                mesmoCredenciado.merge(novoCredenciado);
+            }
+        }
+    }
+
+    console.log("total linhas:  " + trs.length);
+    console.log("total linhas com credenciados: " + total);
+    console.log("total credenciados unicos: " + credenciados.length);
+
 });
