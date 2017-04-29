05f8902f7cd07eb00e2dd3199057cadf71ba420b
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-04-29 11:09:24 -0300

    parser de estados um a um

--- src/scrapper.js
+++ src/scrapper.js
@@ -5,31 +5,65 @@ var fs = require('fs');
 var Parser = require('./Parser.js');
 var Credenciado = require('./Credenciado.js');
 
-obterCredenciados("RJ");
+//var estados = ["AL", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RS", "SC", "SE", "SP", "TO"];
+var estados = ["DF","DF"];
 
-function obterCredenciados(estado) {
-    var url = 'https://seguro2.petrobras.com.br/buscaams/busca.do';
-    var formData = {
-        estado: estado,
-        cidade: "NITEROI",
-        atendimento: 'true',
-        method: 'buscar'
+fetchOneByOne(estados, []);
+
+function fetchOneByOne(estados, credenciadosPorEstado) {
+
+    if(credenciadosPorEstado.length == estados.length){
+
+        reduceCredenciados(credenciadosPorEstado);
+
+    }else{
+
+        var estado = estados[credenciadosPorEstado.length];
+
+        fetch(estado).then(function(body) {
+
+            return parseCredenciados(body)
+
+        }).then(function(novosCredenciados) {
+
+            credenciadosPorEstado.push(novosCredenciados);
+
+            fetchOneByOne(estados, credenciadosPorEstado);
+        });
     }
+}
 
-    console.log("Consultando AMS");
-    request.post({url: url, formData: formData}, function (err, httpResponse, body) {
-        if (err) {
-            return console.error('upload failed:', err);
+function reduceCredenciados(credenciadosPorEstado){
+
+    let todosCredenciados = _.flatten(credenciadosPorEstado);
+    fs.writeFile("credenciados.json", JSON.stringify(todosCredenciados, null, 2));
+}
+
+function fetch(estado) {
+
+    return new Promise(function(resolve, reject){
+
+        var url = 'https://seguro2.petrobras.com.br/buscaams/busca.do';
+        var formData = {
+            estado: estado,
+            atendimento: 'true',
+            method: 'buscar'
         }
 
-        console.log("Realizando parser");
-        var credenciados = parseCredenciados(body);
-        fs.writeFile("credenciados.json", JSON.stringify(credenciados, null, 2));
+        console.log("Consultando AMS - " + estado);
+        request.post({url: url, formData: formData}, function (err, httpResponse, body) {
+            if (err) {
+                reject(httpResponse);
+            }else {
+                resolve(body);
+            }
+        });
     });
 }
 
 function parseCredenciados(body) {
 
+    console.log("Realizando parser");
     var $ = cheerio.load(body);
     var parser = new Parser($);
 
@@ -58,9 +92,9 @@ function parseCredenciados(body) {
         }
     }
 
-    console.log("relatorio:");
-    console.log("total linhas:  " + trs.length);
-    console.log("total linhas com credenciados: " + total);
+    //console.log("relatorio:");
+    //console.log("total linhas:  " + trs.length);
+    //console.log("total linhas com credenciados: " + total);
     console.log("total credenciados unicos: " + credenciados.length);
 
     return credenciados;
