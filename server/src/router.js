54f7031545d56c88b1af4c2d920365aafd2776bb
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-05-09 15:52:53 -0300

    implementacao de router em servidor

---
+++ server/src/router.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var url = require('url');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var staticBasePath = './static';
+var serve = serveStatic(staticBasePath);
+
+
+var createRouter = function (port) {
+    var api = {};
+    var routes = {};
+    var methods = ['GET', 'POST', 'OPTIONS'];
+
+    methods.forEach(function (method) {
+        routes[method] = {};
+        api[method.toLowerCase()] = function (path, fn) {
+            routes[method][path] = fn;
+        };
+    });
+
+    http.createServer(function (req, res) {
+
+        var path = url.parse(req.url).pathname
+        if (!routes[req.method][path]) {
+
+            var done = finalhandler(req, res);
+            return serve(req, res, done);
+
+        }
+        routes[req.method][path](req, res);
+
+    }).listen(port);
+
+    return api;
+};
+
+module.exports = createRouter;
--- server/src/server.js
+++ server/src/server.js
@@ -1,40 +1,26 @@
 var http = require('http');
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
 var url = require('url');
+var router = require('./router');
 
-var staticBasePath = './static';
+var app = router(3000);
 
-var serve = serveStatic(staticBasePath);
-
-var server = http.createServer(function(req, res){
+app.get('/search', function (req, res) {
 
     var q = url.parse(req.url, true);
+    console.log(q.query.q);
+    var formData = { "query":{ "fuzzy": {"_all": q.query.q}}} ;
 
-    if(q.pathname === '/search') {
+    var options = {
+        host: 'localhost',
+        port: 9200,
+        path: '/ams/credenciado/_search',
+        method: 'POST'
+    };
 
-        console.log(q.query.q);
-        var formData = { "query":{ "fuzzy": {"_all": q.query.q}}} ;
+    var esReq = http.request(options, function (esRes) {
+        esRes.pipe(res);
+    });
 
-        var options = {
-            host: 'localhost',
-            port: 9200,
-            path: '/ams/credenciado/_search',
-            method: 'POST'
-        };
-
-        var esReq = http.request(options, function (esRes) {
-            esRes.pipe(res);
-        });
-
-        esReq.write(JSON.stringify(formData));
-        esReq.end();
-    }
-    else {
-        var done = finalhandler(req, res);
-        serve(req, res, done);
-    }
+    esReq.write(JSON.stringify(formData));
+    esReq.end();
 });
-
-server.listen(3000);
-
