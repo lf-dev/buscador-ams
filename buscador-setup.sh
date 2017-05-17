256d30475345899c0e54b4c6c47c17aea4758489
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-05-17 14:30:41 -0300

    setup para ambiente linux aws

---
+++ buscador-setup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# ---------------
+# INSTALACAO JAVA
+# ---------------
+# adaptado de: https://gist.github.com/rtfpessoa/17752cbf7156bdf32c59
+
+java_base_version="8"
+java_sub_version="73"
+java_base_build="02"
+
+java_version="${java_base_version}u${java_sub_version}"
+java_build="b${java_base_build}"
+java_version_with_build="${java_version}-${java_build}"
+
+# -download
+wget --no-cookies --header "Cookie: gpw_e24=xxx; oraclelicense=accept-securebackup-cookie;" "http://download.oracle.com/otn-pub/java/jdk/${java_version_with_build}/jdk-${java_version}-linux-x64.rpm"
+# -install
+sudo rpm -i jdk-${java_version}-linux-x64.rpm
+# -instalacao alternativa
+sudo /usr/sbin/alternatives --install /usr/bin/java java /usr/java/jdk1.${java_base_version}.0_${java_sub_version}/bin/java 20000
+# -configura como padrao
+sudo /usr/sbin/alternatives --config java
+# -exporta java home
+export JAVA_HOME=/usr/java/default
+
+# -------------------------
+# INSTALACAO ELASTIC SEARCH
+# -------------------------
+
+sudo rpm -i https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.rpm
+
+# -configura ES como servico e configurar para iniciar automaticamente
+sudo chkconfig --add elasticsearch
+sudo chkconfig --levels 3 elasticsearch on
+
+# -configura memoria maxima de 100Mb
+sudo sed -i 's/Xmx2g/Xmx100m/g' /etc/elasticsearch/jvm.options
+sudo sed -i 's/Xms2g/Xms100m/g' /etc/elasticsearch/jvm.options
+
+# -inicia servico do ES
+sudo service elasticsearch start
+
+# -----------------
+# INSTALACAO NODEJS
+# -----------------
+
+# -instala o nvm (node version manager)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+
+# -instala o node js
+nvm install 6.10.2
+
+# -pm2
+npm -g install pm2
+
+# --------------
+# INSTALACAO GIT
+# --------------
+
+sudo yum install git
+
+# -clona o projeto
+git clone https://github.com/lf-dev/buscador-ams.git
+
+# -------------------
+# INSTALACAO SCRAPPER
+# -------------------
+
+npm install --prefix ./buscador-ams/ams-scrapper/
+
+node src/scrapper.js
+
+curl -H "Content-Type: application/json" -XPOST 'localhost:9200/ams/credenciado/_bulk?pretty&refresh' --data-binary "@credenciados.json"
+
+# TODO: tratar falha no site da AMS
+# TODO: agendar scrapper e carga em cron
+
+# -------------------
+# INSTALACAO SERVIDOR
+# -------------------
+
+npm install --prefix ./buscador-ams/server/
+
+# TODO: Configurar para iniciar servidor automaticamente
