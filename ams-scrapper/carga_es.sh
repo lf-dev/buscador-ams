7ed488a6289cae35769dd18448af98fb87d5809d
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-05-17 20:37:28 -0300

    parametriacao de arquivo de carga para ES

--- ams-scrapper/carga_es.sh
+++ ams-scrapper/carga_es.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+FILE=$1
 
-if test -e credenciados.json
+if [ -e "$FILE" ] && [ ! -z "$FILE" ]
 then
+
   curl -XDELETE 'localhost:9200/ams'
-  curl -H "Content-Type: application/json" -XPOST 'localhost:9200/ams/credenciado/_bulk?pretty&refresh' --data-binary "@credenciados.json"
-  rm credenciados.json
+  curl -H "Content-Type: application/json" -XPOST 'localhost:9200/ams/credenciado/_bulk?pretty&refresh' --data-binary "@$1"
+  rm $1
 fi
