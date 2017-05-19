9cea61e9903b19342c95d89f366040e604ddc031
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-05-19 13:17:01 -0300

    configuracao de crontab para executar script quando iniciar a maquina

--- ams-scrapper/scrapper-setup.sh
+++ ams-scrapper/scrapper-setup.sh
@@ -12,9 +12,6 @@ export NVM_DIR="$HOME/.nvm"
 # -instala o node js
 nvm install 6.10.2
 
-# -pm2
-# npm -g install pm2
-
 # --------------
 # INSTALACAO GIT
 # --------------
@@ -29,13 +26,8 @@ git clone https://github.com/lf-dev/buscador-ams.git
 # -------------------
 
 npm install --prefix buscador-ams/ams-scrapper/
-# pm2 start ./buscador-ams/ams-scrapper/ecosystem.config.js
-
-# -configura pm2 para iniciar servidor no boot
-# sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v6.10.2/bin /home/ec2-user/.nvm/versions/node/v6.10.2/lib/node_modules/pm2/bin/pm2 startup amazon -u ec2-user --hp /home/ec2-user
-# pm2 save
 
 # -configura crontab para atualizar Elastic Search quando existir arquivo de credenciados
-# echo "* * * * * /home/ec2-user/buscador-ams/ams-scrapper/carga_es.sh /home/ec2-user/buscador-ams/ams-scrapper/credenciados.json" > es_cron
-# crontab es_cron
-# rm es_cron
+echo "@reboot /home/ec2-user/buscador-ams/ams-scrapper/run_scrapper.sh" > es_cron
+crontab es_cron
+rm es_cron
