66a6884224aabed3c3ee4091aaddd0819e6bd427
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-05-29 10:21:24 -0300

    versao preliminar de script para extrair e exportar dados para elasticsearch

---
+++ ams-scrapper/script/exportar.js
@@ -0,0 +1,48 @@
+var fs = require('fs'),
+    readline = require('readline');
+
+var rd = readline.createInterface({
+    input: fs.createReadStream('credenciados.json'),
+    console: false
+});
+
+var handlers = [new CidadeHandler()];
+
+rd.on('line', function(line) {
+
+    var json = JSON.parse(line);
+
+    handlers.forEach(function(handler) {
+        handler.export(line, json);
+    });
+});
+
+rd.on('close', function() {
+
+    handlers.forEach(function(handler) {
+
+        var stream = fs.createWriteStream(handler.filename);
+
+        var index = 1;
+        handler.elements.forEach(function(element) {
+            stream.write('{"index":{"_id":"' + (index+1) + '"} }\n');
+            stream.write(JSON.stringify(element)+'\n');
+            index++;
+        });
+        stream.end();
+    });
+
+});
+
+
+function CidadeHandler() {
+    this.elements = new Set();
+    this.filename = "cidades.json";
+
+    this.export = function(line, json){
+        var self = this;
+        json.credenciado.enderecos.forEach(function(e) {
+            self.elements.add(e.cidade);
+        });
+    }
+}
