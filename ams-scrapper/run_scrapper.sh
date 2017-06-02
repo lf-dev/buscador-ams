6e65cbf66c6820e3006c96e93308ae2127758050
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-06-02 12:45:39 -0300

    Abortando estrategia de extrair propriedades de credenciado para aprimorar busca

--- ams-scrapper/run_scrapper.sh
+++ ams-scrapper/run_scrapper.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 CREDENCIADOS=credenciados_es_batch.json
-ESTADOS=estados_es_batch.json
-CIDADES=cidades_es_batch.json
-BAIRROS=bairros_es_batch.json
-ESPECIALIDADES=especialidades_es_batch.json
 
 if [ ! -v ES_HOST ]
 then
@@ -17,7 +13,6 @@ fi
 cd /home/ec2-user/buscador-ams/ams-scrapper
 /home/ec2-user/.nvm/versions/node/v6.10.2/bin/node src/scrapper.js
 /home/ec2-user/.nvm/versions/node/v6.10.2/bin/node script/exportar-credenciados-elasticsearch.js
-/home/ec2-user/.nvm/versions/node/v6.10.2/bin/node script/exportar-propriedades-elasticsearch.js
 
 if [ -e "$CREDENCIADOS" ] && [ ! -z "$CREDENCIADOS" ]
 then
@@ -25,14 +20,5 @@ then
   curl -XDELETE "$ES_HOST:9200/ams"
   curl -H "Content-Type: application/json" -XPOST "$ES_HOST:9200/ams/credenciado/_bulk?pretty&refresh" --data-binary "@$CREDENCIADOS"
 
-  curl -H "Content-Type: application/json" -XPOST "$ES_HOST:9200/ams/estado/_bulk?pretty&refresh" --data-binary "@$ESTADOS"
-  curl -H "Content-Type: application/json" -XPOST "$ES_HOST:9200/ams/cidade/_bulk?pretty&refresh" --data-binary "@$CIDADES"
-  curl -H "Content-Type: application/json" -XPOST "$ES_HOST:9200/ams/bairro/_bulk?pretty&refresh" --data-binary "@$BAIRROS"
-  curl -H "Content-Type: application/json" -XPOST "$ES_HOST:9200/ams/especialidade/_bulk?pretty&refresh" --data-binary "@$ESPECIALIDADES"
-
   rm $CREDENCIADOS
-  rm $ESTADOS
-  rm $CIDADES
-  rm $BAIRROS
-  rm $ESPECIALIDADES
 fi
