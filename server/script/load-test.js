7f90ccd29a6fcaf2ac8cc4d9c556847276c63eef
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-06-02 21:30:04 -0300

    versao preliminar para teste de carga

---
+++ server/script/load-test.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+const siteUrl = "http://localhost:3000";
+const resources = ['/js/app.js', '/css/styles.css'];
+
+function User() {
+
+    this.events = [];
+
+}
+
+User.prototype._request = function(url, resolve) {
+    request(url, function(error, response, body) {
+
+        if(response.statusCode != 200){
+            console.error("erro ao requisitar " + url);
+        }
+        resolve();
+    });
+}
+
+User.prototype.land = function() {
+
+    var event = {
+        type: 'land',
+        start: Date.now()
+    }
+    var self = this;
+    var start = new Date().getTime();
+
+    new Promise(function(resolve, reject) {
+        self._request(siteUrl, resolve);
+
+    }).then(function(resolve, reject) {
+
+        var promises = resources.map(function(resource) {
+            return new Promise(function(resolve, reject) {
+                self._request(siteUrl + resource, resolve);
+            });
+        });
+
+        Promise.all(promises).then(function() {
+            event.end = Date.now();
+            self.events.push(event);
+        });
+    });
+}
+
+var u = new User();
+u.land();
