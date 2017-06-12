7f09cd1a62207747490699cb89a4ea691be0d447
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-06-11 21:10:52 -0300

    alteracao da query DSL elasticsearch em server para remover o profixo de credenciado das propropriedades'

--- server/src/server.js
+++ server/src/server.js
@@ -19,17 +19,17 @@ app.get('/search', function (req, res) {
                         "multi_match": {
                             "query": q.query.q,
                             "fields": [
-                                "credenciado.pessoa.id^4",
-                                "credenciado.enderecos.telefones^4",
-                                "credenciado.pessoa.nome^3",
-                                "credenciado.enderecos.cidade^2",
+                                "pessoa.id^4",
+                                "enderecos.telefones^4",
+                                "pessoa.nome^3",
+                                "enderecos.cidade^2",
                                 "_all" ],
                             "type": "cross_fields"
                         }
                     },
                     {
                         "match": {
-                            "credenciado.enderecos.especialidades": {
+                            "enderecos.especialidades": {
                                 "query": q.query.q,
                                 "fuzziness": "AUTO"
                             }
