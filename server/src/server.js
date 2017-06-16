76a42aea0edee0e38ea4b443bd032398011e13ee
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-06-16 20:18:01 -0300

    beautify em server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,54 +4,54 @@ var router = require('./router');
 
 var app = router(3000);
 
-app.get('/search', function (req, res) {
+app.get('/search', function(req, res) {
 
-    var pageSize = 10;
-    var q = url.parse(req.url, true);
+  var pageSize = 10;
+  var q = url.parse(req.url, true);
 
-    var query = {
-        "from": q.query.from*pageSize,
-        "size": pageSize,
-        "query": {
-            "bool": {
-                "should": [
-                    {
-                        "multi_match": {
-                            "query": q.query.q,
-                            "fields": [
-                                "pessoa.id^4",
-                                "enderecos.telefones^4",
-                                "pessoa.nome^3",
-                                "enderecos.cidade^2",
-                                "_all" ],
-                            "type": "cross_fields"
-                        }
-                    },
-                    {
-                        "match": {
-                            "enderecos.especialidades": {
-                                "query": q.query.q,
-                                "fuzziness": "AUTO"
-                            }
-                        }
-                    }
-                ]
+  var query = {
+    "from": q.query.from * pageSize,
+    "size": pageSize,
+    "query": {
+      "bool": {
+        "should": [{
+            "multi_match": {
+              "query": q.query.q,
+              "fields": [
+                "pessoa.id^4",
+                "enderecos.telefones^4",
+                "pessoa.nome^3",
+                "enderecos.cidade^2",
+                "_all"
+              ],
+              "type": "cross_fields"
             }
-        }
-    };
+          },
+          {
+            "match": {
+              "enderecos.especialidades": {
+                "query": q.query.q,
+                "fuzziness": "AUTO"
+              }
+            }
+          }
+        ]
+      }
+    }
+  };
 
 
-    var options = {
-        host: 'localhost',
-        port: 9200,
-        path: '/ams/credenciado/_search',
-        method: 'POST'
-    };
+  var options = {
+    host: 'localhost',
+    port: 9200,
+    path: '/ams/credenciado/_search',
+    method: 'POST'
+  };
 
-    var esReq = http.request(options, function (esRes) {
-        esRes.pipe(res);
-    });
+  var esReq = http.request(options, function(esRes) {
+    esRes.pipe(res);
+  });
 
-    esReq.write(JSON.stringify(query));
-    esReq.end();
+  esReq.write(JSON.stringify(query));
+  esReq.end();
 });
