8a9df834f2c340828287344e8e81c3f51fab9f0f
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-07-25 13:48:14 -0300

    log do momento de reinicializacao do ES pelo keep alive

--- server/enviroment/keep_alive_es.sh
+++ server/enviroment/keep_alive_es.sh
@@ -3,6 +3,6 @@ service=elasticsearch
 
 if (( $(ps -ef | grep -v grep | grep $service | wc -l) == 0 ))
 then
-  echo "iniciando $service !"
+  echo "$(date) iniciando $service !"
   sudo service $service start
 fi
