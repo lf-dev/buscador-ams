1366f7fa508f4b4fef2f02964f9630708980bd0b
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2017-07-25 12:47:02 -0300

    correcao em instalacao de rpm java

--- server/enviroment/server-setup.sh
+++ server/enviroment/server-setup.sh
@@ -7,7 +7,7 @@
 wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 
 # -install
-sudo rpm -i jdk-8u131-linux-x64
+sudo rpm -i jdk-8u131-linux-x64.rpm
 
 # -exporta java home
 export JAVA_HOME=/usr/java/default
